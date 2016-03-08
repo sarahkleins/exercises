bc628ec885cf1d0a6c0775345184e4d1288bc76c
Sarah Kleinman <kleinman.sarah@gmail.com>
2016-03-08 15:22:09 -0500

    Add isDivisible.js

---
+++ codewars/isDivisible.js
@@ -0,0 +1,18 @@
+/* Instructions */
+
+/* is Divisible */
+
+// Create a function isDivisible(n,x,y) 
+//that checks if a number n is divisible by two numbers x AND y
+
+function isDivisible(n, x, y) {
+ if ((n % x === 0) && (n % y === 0)) {
+   return true;
+  } else { 
+   return false;
+  }
+}
+
+function isDivisible(n,x,y) {
+  return ((n % x === 0) && (n % y === 0)) ? true : false;
+}
