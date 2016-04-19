3b0355607022c9ea2e905b885f1881a42f342dfd
Sarah Kleinman <kleinman.sarah@gmail.com>
2016-04-19 12:18:57 -0400

    Add numSum.js

---
+++ codewars/numSum.js
@@ -0,0 +1,33 @@
+/* Instructions */
+
+/*
+Given two integers, which can be positive and negative, 
+find the sum of all the numbers between including them too and return it. 
+If both numbers are equal return a or b.
+*/
+
+/*
+Example: 
+GetSum(1, 0) == 1   // 1 + 0 = 1
+GetSum(1, 2) == 3   // 1 + 2 = 3
+GetSum(0, 1) == 1   // 0 + 1 = 1
+GetSum(1, 1) == 1   // 1 Since both are same
+GetSum(-1, 0) == -1 // -1 + 0 = -1
+GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+*/
+
+function numSum(a,b) {
+  var sum = 0;
+  if (a < b) {
+    for (var i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else if (a > b) {
+    for (var i = a; i >= b; i--) {
+      sum += i;
+    }
+  } else {
+    return a;
+  }
+  return sum;
+}
