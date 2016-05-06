a959bca42eea20d8431265bb721907e4a7a8585e
Sarah Kleinman <kleinman.sarah@gmail.com>
2016-05-06 10:28:46 -0400

    Add recursion.js

---
+++ hackerrank/recusion.js
@@ -0,0 +1,11 @@
+function processData(input) {
+    
+  console.log(factorial(input));
+    
+  function factorial(n) {
+    if (n === 1) {
+      return 1;
+    }
+    return n * factorial(n-1);
+  }
+}
